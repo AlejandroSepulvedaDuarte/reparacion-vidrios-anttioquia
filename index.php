ce5a81357fb47bb1365522147539e1496df4e20b
Alejandro Sepúlveda Duarte <alejosepulveda981@gmail.com>
2020-11-16 19:20:55 -0500

    se  modifico la pagina index.

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Reparación Vidrios Antioquia</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    
+</head>
+
+<body>
+    <div class="container">
+
+        <header class="superior">
+
+            <h1>Reparación De Vidrios Antioquia</h1>
+            
+            <nav class="menu">
+                <ul class="opciones">
+                    <li class="activo"><a href="index.php">CONOCENOS</a></li>
+                    
+                    <li ><a href="index2.php">SERVICIOS</a></li>
+                    
+                    <li ><a href="index3.php">IMAGENES </a></li>
+                </ul>
+            </nav>
+            <div class="logo">
+                <img src="images/logo.jpg" class="imagescaja1" width="300" height="260">
+            </div>
+        </header>
+        <br>
+
+        <section class="cuerpo">
+            <h2 class="subtitulo">Bienvenidos! Te invitamos a que conoscas nuestro trabajo</h2>
+            
+            <div class="texto">
+                <p>
+                    Somos especialistas en la reparación de vidrios automotrices de vehículos particulares y de carga
+                    donde te ofrecemos conocimiento, experiencia y honestidad en todo el valle de aburrá y Antioquia.
+                </p>
+                <br>
+                <p>
+                    Ofrecemos garantía por los servicios realizados como: reparacíon y desmanchado de los vidrios frontales,
+                    laterales y traseros de tu vehículo.
+                </p>
+                <br>
+                <p>
+                    Evitamos que cambies los cristales conservando los originales, minimizando el costo elevado, tiempo 
+                    y las inevitables multas de transito causadas por este tipo de daños 
+                    conocidos como: Trizaduras, Estrellas, Media luna, Ojo de buhey y Rallones.
+                </p>
+                <br>
+                <p>
+                    Contamos con herramientas y productos de alta tecnología que garantizan servicios de alta calidad y
+                    conformidad de nuestros clientes.
+                </p>
+            </div>
+            <article class="caja">
+               <img src="images/vidrios.jpg" alt="">
+            </article>
+        </section>
+        <footer class="pie">
+            <div class="contacto">
+                <p class="siguenos"> Mayor información en:
+                    <br>
+                    Messenger o Whatsapp
+                </p>
+                
+                <a href="https://api.whatsapp.com/send?phone=+573024297882&text=¿Qué servicios ofrecen?" target="_blank">
+                  <img src="icon/Whatsap.jpg" width="50px" height="50px" title="Enviar mensaje de whatsapp" >
+                </a>
+                <a href="https://m.me/marino.quinteroduarte" target="_blank">
+                  <img src="icon/messenger.jpg" title="Enviar mensaje por messenger" width="50px" height="50px">
+                </a>
+            </div>   
+        </footer>
+
+
+    </div>
+</body>
+
+</html>
---
+++ index3.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+    <title>Reparación Vidrios Antioquia</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    
+</head>
+
+<body>
+    <div class="container">
+
+        <header class="superior">
+
+            <h1>Imagenes</h1>
+            
+            <nav class="menu">
+                <ul class="opciones">
+                    <li ><a href="index.php">CONOCENOS</a></li>
+                    <li ><a href="index2.php">SERVICIOS</a></li>
+                    <li class="activo"> <a href="index3.php">IMAGENES </a></li>
+                </ul>
+            </nav>
+            <div class="logo">
+                <img src="images/logo.jpg" class="imagescaja1" width="300" height="260">
+            </div>
+        </header>
+        <br>
+
+        <section class="cuerpo">
+            <h2 class="subtitulo">Imagenes de trabajos y resultados reales </h2>
+            
+            <article class="caja1">
+                <img src="images/imagen3.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen4.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen5.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen6.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen8.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen9.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen12.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/imagen13.PNG" >
+            </article>
+            <article class="caja1">
+                <img src="images/polichado.jpg" >
+            </article>
+           
+        </section>
+        <footer class="pie">
+            <div class="contacto">
+                <p class="siguenos">Escribenos y con gusto de atenderemos </p>
+                <a href="https://api.whatsapp.com/send?phone=+573024297882&text=¿Qué servicios ofrecen?" target="_blank">
+                  <img src="icon/Whatsap.jpg" width="50px" height="50px" title="Enviar mensaje de whatsapp" >
+                </a>
+                <a href="https://m.me/marino.quinteroduarte" target="_blank">
+                  <img src="icon/messenger.jpg" title="Enviar mensaje por messenger" width="50px" height="50px">
+                </a>
+            </div>   
+        </footer>
+
+
+    </div>
+</body>
+
+</html>
